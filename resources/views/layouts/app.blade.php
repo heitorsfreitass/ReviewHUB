220328b5523978ab7cfc7be4f6a598c54c3ac92b
Heitor Freitas <freitasheitor102@gmail.com>
2026-03-13 11:35:20 -0300

    referenced the correct path to scss

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet">
 
     <!-- CSS próprio -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
 
     @stack('styles')
 </head>
